5dab696985513345b3c8a4a38510ea0f8e1c3b23
daniele spina <danielespina9292@gmail.com>
2022-12-21 17:08:28 +0100

    add style form

--- src/component/authentication/Login.js
+++ src/component/authentication/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import "./Login.css";
+import { useDispatch, useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import { Container } from "@mui/material";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -26,41 +28,41 @@ const Login = () => {
   return (
     !isLoggedUser && (
       <>
-        <div className="form">
-          <div className="title">Welcome</div>
-          <div className="subtitle">Let's Login your account!</div>
-          <div className="input-container ic1">
-            <input
-              className="input"
-              id="username"
-              type="text"
-              placeholder="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <div className="cut"></div>
-            <label className="placeholder">
-              Username
-            </label>
-          </div>
-          <div className="input-container ic2">
-            <input
-              id="password"
-              type="password"
-              placeholder="password"
-              className="input"
-              value={psw}
-              onChange={(e) => setPsw(e.target.value)}
-            />
-            <div className="cut"></div>
-            <label  className="placeholder">
-              Password
-            </label>
-          </div>
-          <button type="text" className="submit" onClick={login}>
-            submit
-          </button>
-        </div>
+        <Container maxWidth="sm">
+          <Box>
+            <div className="form">
+              <div className="title">Welcome</div>
+              <div className="subtitle">Let's Login your account!</div>
+              <div className="input-container ic1">
+                <input
+                  className="input"
+                  id="username"
+                  type="text"
+                  placeholder="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <div className="cut"></div>
+                <label className="placeholder">Username</label>
+              </div>
+              <div className="input-container ic2">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                  className="input"
+                  value={psw}
+                  onChange={(e) => setPsw(e.target.value)}
+                />
+                <div className="cut"></div>
+                <label className="placeholder">Password</label>
+              </div>
+              <button type="text" className="submit" onClick={login}>
+                submit
+              </button>
+            </div>
+          </Box>
+        </Container>
       </>
     )
   );
